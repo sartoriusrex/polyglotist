a0d78ee39ee2c0ff19ca0ba03a58ca4ed78ff4b9
sartoriusrex <dmai.developer@gmail.com>
2020-05-28 14:46:00 -1000

    type twenty function

--- src/server/crawler/twenty.ts
+++ src/server/crawler/twenty.ts
@@ -1,66 +1,69 @@
-// const crawlTwenty = async function (page, url, language) {
-//   try {
-//     //
-//     let results = [];
-//     const RandomArticleUrls = await page.$$eval(
-//       'article a:not([class])',
-//       (aTags, url) => {
-//         let urls = aTags
-//           .filter((a) => a.getAttribute('href').slice(0, 1) === '/')
-//           .map((a) => url.slice(0, -1) + a.getAttribute('href'));
-//         let max = urls.length;
-//         let numArticles = 3;
-//         const getRandomInt = (max) =>
-//           Math.floor(Math.random() * Math.floor(max));
+const crawlTwenty = async function (page: any, url: string, language: string) {
+  try {
+    //
+    let results = [];
+    const RandomArticleUrls = await page.$$eval(
+      'article a:not([class])',
+      (aTags: any, url: string) => {
+        let urls = aTags
+          .filter((a: any) => a.getAttribute('href').slice(0, 1) === '/')
+          .map((a: any) => url.slice(0, -1) + a.getAttribute('href'));
+        let max = urls.length;
+        let numArticles = 3;
+        const getRandomInt = (max: number) =>
+          Math.floor(Math.random() * Math.floor(max));
 
-//         let randomIndexes = Array(numArticles)
-//           .fill(1)
-//           .map((num) => getRandomInt(max))
-//           .sort((a, b) => a > b)
-//           .map((num, i, arr) => {
-//             if (i > 0 && num === arr[i - 1]) return ++num;
-//             return num;
-//           });
+        let randomIndexes = Array(numArticles)
+          .fill(1)
+          .map((num: number) => getRandomInt(max))
+          .sort((a: number, b: number) => a - b)
+          .map((num: number, i: number, arr: any[]) => {
+            if (i > 0 && num === arr[i - 1]) return ++num;
+            return num;
+          });
 
-//         return randomIndexes.map((index) => urls[index]);
-//       },
-//       url
-//     );
+        return randomIndexes.map((index: number) => urls[index]);
+      },
+      url
+    );
 
-//     for (let url of RandomArticleUrls) {
-//       await page.goto(url);
+    for (let url of RandomArticleUrls) {
+      await page.goto(url);
 
-//       try {
-//         const title = await page.$eval('h1.nodeheader-title', title => title.innerText );
+      try {
+        const title = await page.$eval(
+          'h1.nodeheader-title',
+          (title: any) => title.innerText
+        );
 
-//         const body = await page.$eval('.lt-endor-body.content', (body) => {
-//           let children = Array.from(body.children);
-//           let allowedTags = ['P', 'H2'];
+        const body = await page.$eval('.lt-endor-body.content', (body: any) => {
+          let children = Array.from(body.children);
+          let allowedTags = ['P', 'H2'];
 
-//           const allowedChildren = children.filter((element) =>
-//             allowedTags.includes(element.tagName)
-//           );
+          const allowedChildren = children.filter((element: any) =>
+            allowedTags.includes(element.tagName)
+          );
 
-//           return allowedChildren.map((element) => [
-//             element.tagName,
-//             element.innerText,
-//           ]);
-//         });
+          return allowedChildren.map((element: any) => [
+            element.tagName,
+            element.innerText,
+          ]);
+        });
 
-//         results.push({ title, url, body, language });
-//       } catch (err) {
-//         console.log(err);
-//         return { error: 'Failed to get article Titles or Bodies from Twenty.fr'}
-//       }
-//     }
+        results.push({ title, url, body, language });
+      } catch (err) {
+        console.log(err);
+        return {
+          error: 'Failed to get article Titles or Bodies from Twenty.fr',
+        };
+      }
+    }
 
-//     return results;
-//   } catch (err) {
-//     console.log(err);
-//     return { error: 'Failed to get article URLs from Twenty.fr'}
-//   }
-// };
+    return results;
+  } catch (err) {
+    console.log(err);
+    return { error: 'Failed to get article URLs from Twenty.fr' };
+  }
+};
 
-// module.exports = crawlTwenty;
-
-export default {};
+export default crawlTwenty;
