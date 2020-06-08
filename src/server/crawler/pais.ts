a6c3c14c935d6835b04466d22f06b8dccc8ae8b5
sartoriusrex <dmai.developer@gmail.com>
2020-06-08 13:56:35 -1000

    update pais to oo structure with external references to itnerfaces

--- src/server/crawler/pais.ts
+++ src/server/crawler/pais.ts
@@ -1,214 +1,186 @@
-interface Error {
-  error: string;
-}
+import { Error, Crawler } from './interfaces';
 
-export const grabURLs = async function (page: any, url: string) {
-  try {
-    // Grab only center column articles that specifically match this query
-    const randomArticleUrls = await page.$$eval(
-      'article',
-      (articles: any, url: string) => {
-        // filter out articles 'en vivo' since they're a bit complicated for scraping (iframe with another url - may do these later)
+const paisCrawler: Crawler = {
+  grabURLs: async function (page: any, url: string) {
+    try {
+      // Grab only center column articles that specifically match this query
+      const randomArticleUrls = await page.$$eval(
+        'article',
+        (articles: any, url: string) => {
+          // filter out articles 'en vivo' since they're a bit complicated for scraping (iframe with another url - may do these later)
 
-        const articlesNoVivo = articles.filter(
-          (element: any) =>
-            !element.querySelector('.kicker span') &&
-            element.querySelector('h2 a')
-        );
-
-        let urls = articlesNoVivo
-          .map((element: any) => element.querySelector('h2 a').href)
-          .filter(
-            (href: string) =>
-              href.slice(8, 14) === 'elpais' && !href.includes('album')
+          const articlesNoVivo = articles.filter(
+            (element: any) =>
+              !element.querySelector('.kicker span') &&
+              element.querySelector('h2 a')
           );
 
-        // Shuffle the array in place: From https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-        function shuffleArray(array: any) {
-          for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
+          let urls = articlesNoVivo
+            .map((element: any) => element.querySelector('h2 a').href)
+            .filter(
+              (href: string) =>
+                href.slice(8, 14) === 'elpais' && !href.includes('album')
+            );
+
+          // Shuffle the array in place: From https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+          function shuffleArray(array: any) {
+            for (let i = array.length - 1; i > 0; i--) {
+              const j = Math.floor(Math.random() * (i + 1));
+              [array[i], array[j]] = [array[j], array[i]];
+            }
           }
-        }
 
-        shuffleArray(urls);
+          shuffleArray(urls);
 
-        const max = urls.length;
-        const numArticlesChoice = 3;
-        const numArticles = Math.min(max, numArticlesChoice);
+          const max = urls.length;
+          const numArticlesChoice = 3;
+          const numArticles = Math.min(max, numArticlesChoice);
 
-        return urls.slice(0, numArticles);
-      },
-      url
-    );
+          return urls.slice(0, numArticles);
+        },
+        url
+      );
 
-    return randomArticleUrls;
-  } catch (err) {
-    console.log(err);
-    return { error: `Failed to get article URLs from ${url}` };
-  }
-};
+      return randomArticleUrls;
+    } catch (err) {
+      console.log(err);
+      return { error: `Failed to get article URLs from ${url}` };
+    }
+  },
+  grabTitle: async function (page: any, url: string) {
+    let title;
 
-export const grabTitle = async function (page: any, url: string) {
-  let title;
+    try {
+      title = await page.$eval('h1', (title: any) => title.innerText);
+    } catch (err) {
+      console.log(err);
+      console.log(`\n\n${url}\n\n`);
+    }
 
-  try {
-    title = await page.$eval('h1', (title: any) => title.innerText);
-  } catch (err) {
-    console.log(err);
-    console.log(`\n\n${url}\n\n`);
-  }
+    return title;
+  },
+  grabDate: async function (page: any, url: string) {
+    let date: string;
+    const months: { [key: string]: string } = {
+      ENE: 'january',
+      FEB: 'february',
+      MAR: 'march',
+      ABR: 'april',
+      MAY: 'may',
+      JUN: 'june',
+      JUL: 'july',
+      AGO: 'august',
+      SEP: 'september',
+      OCT: 'october',
+      NOV: 'november',
+      DIC: 'december',
+    };
 
-  return title;
-};
+    try {
+      date = await page.$eval(
+        '.a_ti',
+        (timeElement: any, months: { [key: string]: string }) => {
+          const dateText = timeElement.innerText.slice(0, 11);
+          let dateTimeObject = new Date(dateText);
 
-export const grabDate = async function (page: any, url: string) {
-  let date: string;
-  const months: { [key: string]: string } = {
-    ENE: 'january',
-    FEB: 'february',
-    MAR: 'march',
-    ABR: 'april',
-    MAY: 'may',
-    JUN: 'june',
-    JUL: 'july',
-    AGO: 'august',
-    SEP: 'september',
-    OCT: 'october',
-    NOV: 'november',
-    DIC: 'december',
-  };
+          if (typeof dateTimeObject === 'string') {
+            const dateTextArray = dateText.split(' ');
+            const newDateText = dateTextArray
+              .map((el: string) => {
+                if (months[el]) return months[el];
+                return el;
+              })
+              .join(' ');
 
-  try {
-    date = await page.$eval(
-      '.a_ti',
-      (timeElement: any, months: { [key: string]: string }) => {
-        const dateText = timeElement.innerText.slice(0, 11);
-        let dateTimeObject = new Date(dateText);
+            dateTimeObject = new Date(newDateText);
+          }
 
-        if (typeof dateTimeObject === 'string') {
-          const dateTextArray = dateText.split(' ');
-          const newDateText = dateTextArray
-            .map((el: string) => {
+          return dateTimeObject.toLocaleString();
+        },
+        months
+      );
+
+      return date;
+    } catch (err) {
+      console.log('\n failed to grab .a_ti. Will now attempt 2nd capture');
+    }
+
+    try {
+      date = await page.$eval(
+        'time',
+        (dateElement: any, months: { [key: string]: string }) => {
+          const dateText = dateElement.innerText.slice(0, 11);
+
+          const dateTextInEnglish = dateText
+            .split(' ')
+            .map((el: any) => {
               if (months[el]) return months[el];
               return el;
             })
             .join(' ');
 
-          dateTimeObject = new Date(newDateText);
-        }
+          return new Date(dateTextInEnglish).toLocaleString();
+        },
+        months
+      );
 
-        return dateTimeObject.toLocaleString();
-      },
-      months
-    );
+      return date;
+    } catch (err) {
+      console.log(err);
+      return { error: `Failed to grab the time from ${url}` };
+    }
+  },
+  grabBody: async function (page: any, title: string, url: string) {
+    let body;
 
-    return date;
-  } catch (err) {
-    console.log('\n failed to grab .a_ti. Will now attempt 2nd capture');
-  }
+    // Different article types need to be treated differently
+    try {
+      body = await page.$eval(
+        'article',
+        async (body: any, url: string) => {
+          let desc = null;
+          if (body.querySelector('h1 + h2')) {
+            desc = ['H2', body.querySelector('h1 + h2').innerText];
+          }
 
-  try {
-    date = await page.$eval(
-      'time',
-      (dateElement: any, months: { [key: string]: string }) => {
-        const dateText = dateElement.innerText.slice(0, 11);
+          const articleBodyContainer =
+            body.querySelector('.article_body') ||
+            body.querySelector('.articulo-cuerpo') ||
+            body.querySelector('.cuerpo');
 
-        const dateTextInEnglish = dateText
-          .split(' ')
-          .map((el: any) => {
-            if (months[el]) return months[el];
-            return el;
-          })
-          .join(' ');
+          const allowedTags = ['P', 'H2', 'H3', 'BLOCKQUOTE', 'UL'];
+          let articleText;
 
-        return new Date(dateTextInEnglish).toLocaleString();
-      },
-      months
-    );
+          if (articleBodyContainer !== null) {
+            articleText = Array.from(articleBodyContainer.children)
+              .filter((element: any) => allowedTags.includes(element.tagName))
+              .map((element: any) => {
+                if (element.tagName === 'UL') {
+                  const ulChildren = Array.from(element.children);
 
-    return date;
-  } catch (err) {
-    console.log(err);
-    return { error: `Failed to grab the time from ${url}` };
-  }
+                  return ulChildren.map((el: any) => [
+                    el.tagName,
+                    el.textContent,
+                  ]);
+                }
+                return [element.tagName, element.textContent];
+              });
+          } else {
+            articleText = [['H2', `No Body Found in ${url}`]];
+          }
+
+          if (desc) articleText.unshift(desc);
+          return articleText;
+        },
+        url
+      );
+    } catch (err) {
+      console.log(err);
+      return { error: `Failed to get article bodies from ${url}` };
+    }
+
+    return body;
+  },
 };
 
-export const grabBody = async function (page: any, title: string, url: string) {
-  let body;
-
-  // Different article types need to be treated differently
-  try {
-    body = await page.$eval(
-      'article',
-      async (body: any, url: string) => {
-        let desc = null;
-        if (body.querySelector('h1 + h2')) {
-          desc = ['H2', body.querySelector('h1 + h2').innerText];
-        }
-
-        const articleBodyContainer =
-          body.querySelector('.article_body') ||
-          body.querySelector('.articulo-cuerpo') ||
-          body.querySelector('.cuerpo');
-
-        const allowedTags = ['P', 'H2', 'H3', 'BLOCKQUOTE', 'UL'];
-        let articleText;
-
-        if (articleBodyContainer !== null) {
-          articleText = Array.from(articleBodyContainer.children)
-            .filter((element: any) => allowedTags.includes(element.tagName))
-            .map((element: any) => {
-              if (element.tagName === 'UL') {
-                const ulChildren = Array.from(element.children);
-
-                return ulChildren.map((el: any) => [
-                  el.tagName,
-                  el.textContent,
-                ]);
-              }
-              return [element.tagName, element.textContent];
-            });
-        } else {
-          articleText = [['H2', `No Body Found in ${url}`]];
-        }
-
-        if (desc) articleText.unshift(desc);
-        return articleText;
-      },
-      url
-    );
-  } catch (err) {
-    console.log(err);
-    return { error: `Failed to get article bodies from ${url}` };
-  }
-
-  return body;
-};
-
-const crawlPais = async function (page: any, url: string, language: string) {
-  // go to main page, scrape for desired links to article pages and return 3 random articles, and scrape those pages for their contents;
-  let results = [];
-
-  const randomArticleUrls: string[] | Error = await grabURLs(page, url);
-  if (!Array.isArray(randomArticleUrls)) return randomArticleUrls.error;
-
-  for (let url of randomArticleUrls) {
-    await page.goto(url);
-
-    let title: string | Error = await grabTitle(page, url);
-    if (typeof title !== 'string') title = 'No Title Found';
-
-    let date: string | Error = await grabDate(page, url);
-    if (typeof date !== 'string') date = 'No Date Found';
-
-    let body: string[][] | Error = await grabBody(page, title, url);
-    if (!Array.isArray(body)) body = [['H2', body.error]];
-
-    results.push({ title, date, url, body, language });
-  }
-
-  return results;
-};
-
-export default crawlPais;
+export default paisCrawler;
