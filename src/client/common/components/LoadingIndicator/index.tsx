fd7bba792096626fbe38bf23650a3b682f50673f
sartoriusrex <dmai.developer@gmail.com>
2020-06-29 12:25:16 -1000

    refetch data and uncomment initialize; correct styling for loading circles, since it was targeting other svgs as well

--- src/client/common/components/LoadingIndicator/index.tsx
+++ src/client/common/components/LoadingIndicator/index.tsx
@@ -12,28 +12,19 @@ const LoadingIndicator = () => {
           viewBox='0 0 13.229166 2.6458334'
           height='10'
           width='50'
+          class={styles.svg}
         >
           <defs id='defs2' />
           <metadata id='metadata5'></metadata>
           <g id='layer1'>
-            <circle
-              r='1.3229166'
-              cy='1.3229166'
-              cx='1.3229166'
-              id='path833'
-            />
+            <circle r='1.3229166' cy='1.3229166' cx='1.3229166' id='path833' />
             <circle
               id='path833-8'
               cx='6.6145835'
               cy='1.3229166'
               r='1.3229166'
             />
-            <circle
-              id='path833-9'
-              cx='11.90625'
-              cy='1.3229166'
-              r='1.3229166'
-            />
+            <circle id='path833-9' cx='11.90625' cy='1.3229166' r='1.3229166' />
           </g>
         </svg>
       </div>
