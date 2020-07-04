6fedf1b849bf39ba6a5000d67d7df33362c5c956
sartoriusrex <dmai.developer@gmail.com>
2020-07-03 17:06:07 -1000

    add history to bottomnavbar and test that 4 links appear, that their hrefs are correct, and that they navigate properly, changing the history object with it

--- src/client/features/BottomNavbar/bottomNavbar.test.tsx
+++ src/client/features/BottomNavbar/bottomNavbar.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { render, screen, fireEvent } from 'test-utils';
+import history from '../../app/history';
 // import { renderInRouter } from 'test-router';
 
 import BottomNavbar from './index';
@@ -18,9 +19,33 @@ const initialState = {
 };
 
 describe('Bottom Navbar', () => {
-  test('Renders 4 links', () => {
-    render(<BottomNavbar />, { initialState: initialState });
+  test('Renders 4 links and properly navigates between them.', () => {
+    const { container } = render(<BottomNavbar />, {
+      initialState: initialState,
+    });
 
-    screen.debug();
+    const links: HTMLAnchorElement[] | null = Array.from(
+      container.querySelectorAll('a')
+    );
+
+    if (links !== null) {
+      expect(links.length).toBe(4);
+      expect(links[0].pathname).toBe('/username5/dashboard');
+      expect(links[1].pathname).toBe('/username5/words');
+      expect(links[2].pathname).toBe('/username5/articles');
+      expect(links[3].pathname).toBe('/username5/practice');
+    }
+
+    fireEvent.click(links[1]);
+    expect(history.location.pathname).toBe('/username5/words');
+
+    fireEvent.click(links[2]);
+    expect(history.location.pathname).toBe('/username5/articles');
+
+    fireEvent.click(links[3]);
+    expect(history.location.pathname).toBe('/username5/practice');
+
+    fireEvent.click(links[0]);
+    expect(history.location.pathname).toBe('/username5/dashboard');
   });
 });
