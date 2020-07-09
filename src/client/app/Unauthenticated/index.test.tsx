958e92dfdc6f7d9a4daa3026769bf3a6c9c1fb77
sartoriusrex <dmai.developer@gmail.com>
2020-07-08 19:11:44 -1000

    add tests that test that login and signup pages are rendered correctly and that users are redirected to login page if attempting to access Authenticated app

--- src/client/app/Unauthenticated/index.test.tsx
+++ src/client/app/Unauthenticated/index.test.tsx
@@ -5,24 +5,52 @@ import history from '../history';
 
 import UnauthenticatedApp from './index';
 
+beforeEach(() => {
+  const { container } = render(
+    <TestRouter>
+      <UnauthenticatedApp />
+    </TestRouter>,
+    { initialState: {} }
+  );
+});
+
 describe('Uauthenticated App', () => {
   test('renders the Landing Page', async () => {
-    const { container } = render(
-      <TestRouter>
-        <UnauthenticatedApp />
-      </TestRouter>,
-      { initialState: {} }
-    );
-    console.log(history.location.pathname);
+    expect(
+      screen.getByRole('heading', {
+        name: /A simple app for expanding your foreign vocabulary./i,
+      })
+    ).toBeInTheDocument();
+  });
 
+  test('renders the login page after navigating there', async () => {
     history.push('/login');
 
     expect(history.location.pathname).toBe('/login');
 
-    // expect(history.location.pathname).toBe('/username5/create_account');
+    expect(screen.getByRole('heading', { name: /log in/i })).toBeInTheDocument();
+  });
 
-    // history.push('/username5/settings');
+  test('renders the signup page after navigating there', async () => {
+    history.push('/signup');
 
-    // expect(history.location.pathname).toBe('/username5/create_account');
+    expect(history.location.pathname).toBe('/signup');
+    expect(
+      screen.getByRole('heading', { name: /create account/i })
+    ).toBeInTheDocument();
+  });
+
+  test('renders the login page when attempting to navigate to a dashboard', async () => {
+    history.push('/username5/dashboard');
+
+    expect(history.location.pathname).toBe('/login');
+    expect(screen.getByRole('heading', { name: /log in/i })).toBeInTheDocument();
+  });
+
+  test('renders the NoMatch page when attempting to navigate to a nonexistant route', async () => {
+    history.push('thisRouteDoesntExist');
+    expect(
+      screen.getByRole('heading', { name: /This Page Does Not Exist/i })
+    ).toBeInTheDocument();
   });
 });
