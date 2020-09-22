57550b041d9e96ac8416c5b95e34d886cc7f9786
sartoriusrex <dmai.developer@gmail.com>
2020-09-22 11:02:10 -1000

    modify node-cron job to fetch new articles every 12 hours

--- src/server/initialize_server.ts
+++ src/server/initialize_server.ts
@@ -22,22 +22,20 @@ export default function initializeServer(router: Router) {
     max: 50,
   });
 
-  (async function initializeData() {
-    // await initializeDatabase();
-    // await fetchFreshArticles();
-  })();
+  // (async function initializeData() {
+  //   await initializeDatabase();
+  //   await fetchFreshArticles();
+  // })();
 
-  // after initializing Data, fetch fresh articles at 600 and 1800 every day using node-cron
-  //There seem to be issues with this task - it gets called in an endless loop for some reason, and at a time that does not seem to match the params
-  cron.schedule('* * 6,18 * * *', async () => {
+  // after initializing Data, fetch fresh articles every 12 hours. FYI time is 10 hours ahead of HST
+  cron.schedule("* * */12 * * *", async () => {
     try {
       console.log(`-=-=-=-fetching new articles-=-=-=-`);
 
-      return await fetchFreshArticles()
+      await fetchFreshArticles()
     } catch (err) {
       console.log(`%+%+%+%+ Error %+%+%+%\n${err}`)
     }
-
   });
 
   app.use(bodyParser.json());
