b2c04ba9f93489ddf1c442157d3a142eae74a446
sartoriusrex <dmai.developer@gmail.com>
2020-12-25 12:32:28 +0100

    restyle h1 in articlepage and restyle sections to have less padding; add styles into practicePage

--- src/client/pages/PracticePage/index.tsx
+++ src/client/pages/PracticePage/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import styles from './practicePage.module.scss';
+
 const PracticePage = () => {
   return(
-    <section>
+    <section className={styles.practicePage}>
       <h1>Practice Page</h1>
     </section>
   )
