a533fddced4b7c440cda12099ef3f4b268299286
sartoriusrex <dmai.developer@gmail.com>
2020-12-25 22:17:59 +0100

    add html structure for practice index page

--- src/client/pages/PracticePage/index.tsx
+++ src/client/pages/PracticePage/index.tsx
@@ -5,7 +5,25 @@ import styles from './practicePage.module.scss';
 const PracticePage = () => {
   return(
     <section className={styles.practicePage}>
-      <h1>Practice Page</h1>
+      <h1>Practice Vocabulary</h1>
+      <div className="languageSection">
+        <h2>Languages</h2>
+        {/* List languages, including all */}
+      </div>
+
+      <div className="modeSection">
+        <h2>Mode</h2>
+        <div className="input-group">
+            <input type="radio" id="untimed" name="untimed" value="untimed" defaultChecked />
+            <label htmlFor="untimed">Untimed</label>
+        </div>
+        <div className="input-group">
+            <input type="radio" id="untimed" name="untimed" value="untimed" />
+            <label htmlFor="untimed">Timed</label>
+        </div>
+      </div>
+
+      <button>Start</button>
     </section>
   )
 }
