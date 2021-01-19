5daf1c46428e8af95fc63c9995e4c58e0f1d8e5c
sartoriusrex <dmai.developer@gmail.com>
2021-01-19 11:47:21 +0100

    adjust display to add second or seconds depending of time is 1 ; move style to warn constant

--- src/client/components/Timer/index.tsx
+++ src/client/components/Timer/index.tsx
@@ -3,10 +3,13 @@ import React from 'react';
 import styles from './timer.module.scss';
 
 const Timer = ({time} : {time: number}) => {
+    const warn = time < 10;
+    const seconds = time === 1 ? 'seconds' : 'second';
 
     return (
-        <time className={styles.timer}>Time Remaining: 
-            <span className={ time < 10 ? styles.warning : '' }> {time}</span>
+        <time className={styles.timer}>
+            Time Remaining: 
+            <span className={ warn ? styles.warning : '' }> {time} {seconds}</span>
         </time>
     )
 }
